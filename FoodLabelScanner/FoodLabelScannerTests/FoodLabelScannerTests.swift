0b1278b1dd16c0075128070059ed3e95ab84c569
Dianne <dianne.vincent@stonybrook.edu>
2024-12-31 22:04:45 -0800

    First Commit: Created XCode project

---
+++ FoodLabelScanner/FoodLabelScannerTests/FoodLabelScannerTests.swift
@@ -0,0 +1,17 @@
+//
+//  FoodLabelScannerTests.swift
+//  FoodLabelScannerTests
+//
+//  Created by Dianne Vipasha Vincent on 12/31/24.
+//
+
+import Testing
+@testable import FoodLabelScanner
+
+struct FoodLabelScannerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
